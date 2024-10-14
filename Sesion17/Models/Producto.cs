566fc98568a729edeecda134c1f55490bdd67783
MichaelCasco4 <mecascov@uamv.edu.ni>
2024-10-14 07:21:19 -0600

    Class Product added

---
+++ Sesion17/Models/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sesion17.Models
+{
+    internal class Producto
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }    
+        public string Description { get; set; }
+        public double Price { get; set; }
+
+    }
+}
